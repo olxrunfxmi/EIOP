c47452ee86cda96195696f9ae20c3a1dc82c36b0
olxrunfxmi <191869901+olxrunfxmi@users.noreply.github.com>
2025-07-07 00:55:25 +0100

    Add tradition interactivity

--- js/interaction.js
+++ js/interaction.js
@@ -9,12 +9,54 @@ boxHandlerButtonEl.addEventListener("click", () => {
 	boxInteractiveEl.appendChild(divEl);
 
 	if (boxInteractiveEl.querySelectorAll(".columnbar").length === 1) {
-		const clearBtnEl = document.createElement("button");
-		clearBtnEl.textContent = "Clear";
-		clearBtnEl.classList.add("btn", "clr-btn", "clear");
+		const clearBtnEl = generateElement(
+			"button",
+			["btn", "clr-btn", "clear"],
+			"Clear"
+		);
 		clearBtnEl.addEventListener("click", (e) => {
 			e.currentTarget.parentElement.textContent = "";
 		});
 		boxInteractiveEl.appendChild(clearBtnEl);
 	}
 });
+
+bubbleButtonEl.addEventListener("click", () => {
+	if (traditionEl.querySelectorAll(".circle").length === 0) {
+		const classArr = ["circle"];
+		const sEl = generateElement("div", classArr, "Divisibility Calculator", {
+			name: "type",
+			value: "s",
+		});
+		const mEl = generateElement("div", classArr, "EIOP", {
+			name: "type",
+			value: "m",
+		});
+		const lEl = generateElement("div", classArr, "Odin Project", {
+			name: "type",
+			value: "l",
+		});
+		const xlEl = generateElement("div", classArr, "Recreations", {
+			name: "type",
+			value: "xl",
+		});
+		traditionEl.append(sEl, mEl, lEl, xlEl);
+	} else {
+		const circleEls = traditionEl.querySelectorAll(".circle");
+		circleEls.forEach((circlEl) => {
+			circlEl.remove();
+		});
+	}
+});
+
+function generateElement(element, classArr, textContent, dataObj) {
+	const el = document.createElement(element);
+	el.textContent = textContent;
+	if (classArr) {
+		el.classList.add(...classArr);
+	}
+	if (dataObj) {
+		el.dataset[dataObj.name] = [dataObj.value];
+	}
+	return el;
+}
